7027376a75d6621dedc896d6a7a4948c3b7d5f29
TitanLi <Lisheng0706@gmail.com>
2018-12-14 13:36:08 +0800

    Add MySQL example

--- mysql/app.js
+++ mysql/app.js
@@ -1,8 +1,14 @@
+//Docker run container : docker run --name mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=password -d mysql
+//Mac Install MySQL Client : brew install mysql --client-only --universal
+//Connect to MySQL server : $ mysql -h 127.0.0.1 -u root -p
+//In Mysql docker container : ALTER USER root IDENTIFIED WITH mysql_native_password BY 'password';
+//Create database : create database test;
+
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-    host: '127.0.0.1',
+    host: 'localhost',
     user: 'root',
-    password: 'root',
+    password: 'password',
     database: 'test'
 });
 
