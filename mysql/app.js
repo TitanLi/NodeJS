54c5b30f461c7ba48b08f803d2aacad278a0aa27
TitanLi <lisheng0706@gmail.com>
2019-04-18 19:53:32 +0800

    Add docker run mySQL example

--- mysql/app.js
+++ mysql/app.js
@@ -1,8 +1,12 @@
 //Docker run container : docker run --name mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=password -d mysql
 //Mac Install MySQL Client : brew install mysql --client-only --universal
 //Connect to MySQL server : $ mysql -h 127.0.0.1 -u root -p
+//docker exec -ti 8 bash
+//mysql -uroot -ppassword
 //In Mysql docker container : ALTER USER root IDENTIFIED WITH mysql_native_password BY 'password';
 //Create database : create database test;
+//mac MySQL Client UI install:https://www.mysql.com/products/workbench/
+//Setting workbench:export PATH=$PATH:/Applications/MySQLWorkbench.app/Contents/MacOS
 
 var mysql = require('mysql');
 var connection = mysql.createConnection({
