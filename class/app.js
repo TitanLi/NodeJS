d04a69c8e922f54f695d8a4ea384bfff70159c50
TitanLi <lisheng0706@gmail.com>
2019-11-02 23:46:56 +0800

    Update class example

--- class/app.js
+++ class/app.js
@@ -4,4 +4,8 @@ const mqtt = new MQTT('127.0.0.1:1883');
 
 setInterval(() => {
     mqtt.push();
+    console.log(mqtt.firstClassData);
+    // 127.0.0.1:1883
+    mqtt.firstClassData = 100;
+    // setter: 100
 }, 3000);
--- class/classDemo.js
+++ class/classDemo.js
@@ -1,11 +1,20 @@
 const mqtt = require('mqtt');
 class MQTT {
     constructor(IP) {
+        this.ip = IP;
         this.client = mqtt.connect(`mqtt://${IP}`);
         this.client.on('connect', () => {
             console.log('MQTT connect');
             this.client.subscribe('message');
         });
+    }
+
+    get firstClassData(){
+        return this.ip;
+    }
+
+    set firstClassData(value) {
+        console.log('setter: '+ value);
     }
 
     push() {
