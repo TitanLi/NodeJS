b74a0c615a3c4af0e5de91175c00f0888384825c
TitanLi <lisheng0706@gmail.com>
2019-11-12 08:48:56 +0800

    Add fs stream finish event

--- module/fs/stream.js
+++ module/fs/stream.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
-let readStream = fs.createReadStream('promisesRead.js','utf-8');
-let writeStream = fs.createWriteStream('demo.js','utf-8');
+let readStream = fs.createReadStream('promisesRead.js', 'utf-8');
+let writeStream = fs.createWriteStream('demo.js', 'utf-8');
 
 // readStream.on('data',(data) => {
 //     writeStream.write(data);
@@ -9,3 +9,11 @@ let writeStream = fs.createWriteStream('demo.js','utf-8');
 
 // 與上方功能相同 
 readStream.pipe(writeStream);
+
+readStream.on('end', function () {
+    console.log("End read");
+});
+
+writeStream.on('finish', function () {
+    console.log("Data write");
+});
