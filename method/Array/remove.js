f8ecfd379a40f1a708a7fed748319d4939523e04
TitanLi <lisheng0706@gmail.com>
2019-12-08 17:02:50 +0800

    Add Array prototype

--- method/Array/remove.js
+++ method/Array/remove.js
@@ -1,5 +1,25 @@
-let data = [1,2,3,4,5,6,7];
+// Method 1
+let data = [1, 2, 3, 4, 5, 6, 7];
 
 data = data.filter(item => item !== 7);
 
 console.log(data)
+
+// Method 2
+Array.prototype.indexOf = function (val) {
+    for (var i = 0; i < this.length; i++) {
+        if (this[i] == val) return i;
+    }
+    return -1;
+};
+
+Array.prototype.remove = function (val) {
+    var index = this.indexOf(val);
+    if (index > -1) {
+        this.splice(index, 1);
+    }
+};
+
+let emp = ['abs', 'dsf', 'sdf', 'fd'];
+emp.remove('fd');
+console.log(emp);
