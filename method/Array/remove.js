06fa7004848d7c2a32b79f72e0b33123788d2e39
TitanLi <lisheng0706@gmail.com>
2019-12-13 13:42:50 +0800

    Add array remove method

--- method/Array/remove.js
+++ method/Array/remove.js
@@ -10,6 +10,17 @@ var filtered = array.filter(function(value, index, arr){
 });
 console.log(filtered); // return [6, 7, 8, 9]
 console.log(array); // return [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+// Method 3
+var arr = [1, 2, 3, 4, 5, 5, 6, 7, 8, 5, 9, 0];
+
+for( var i = 0; i < arr.length; i++){ 
+   if ( arr[i] === 5) {
+     arr.splice(i, 1); 
+     i--;
+   }
+}
+console.log(arr);
+// [ 1, 2, 3, 4, 6, 7, 8, 9, 0 ]
 
 // 僅移除一個符合條件的元素
 Array.prototype.indexOf = function (val) {
