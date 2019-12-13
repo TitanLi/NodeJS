29e62a0597c780f1ec89509cbb66b00f4b47be21
TitanLi <lisheng0706@gmail.com>
2019-12-13 13:46:17 +0800

    Add genetic-algorithm #1

--- method/genetic-algorithm/GA.js
+++ method/genetic-algorithm/GA.js
@@ -1,246 +1,126 @@
-class GA {
-    constructor(vnf) {
-        this.compute = [2, 4, 4, 4, 6, 8, 8, 8, 10, 12];
-        this.vnf = vnf;
-        // this.initGenSize = 2;
-        this.initGenSize = this.vnf.length * this.compute.length
-        this.initGen = new Array(this.initGenSize);
-        // 在基因大小中新增計算節點維度
-        for (let i = 0; i < this.initGen.length; i++) {
-            // 新增計算維度
-            this.initGen[i] = new Array(this.compute.length + 1);
-            this.initGen[i][0] = null;
-            // 在計算節點中新增放置空間
-            for (let j = 1; j <= this.compute.length; j++) {
-                // 新增維度
-                this.initGen[i][j] = new Array(0);
-            }
-        }
-        Array.prototype.indexOf = function (val) {
-            for (var i = 0; i < this.length; i++) {
-                if (this[i] == val) return i;
-            }
-            return -1;
-        };
+const compute = [2, 4, 4, 4, 6, 8, 8, 8, 10, 12];
+const vnf = [2, 2, 3, 4, 5, 6, 6];
+const initGenSize = 2;
+let initGen = new Array(initGenSize);
 
-        Array.prototype.remove = function (val) {
-            var index = this.indexOf(val);
-            if (index > -1) {
-                this.splice(index, 1);
-            }
-        };
-    }
-
-    init() {
-        // this.compute.sort(function (a, b) { return b - a });
-        console.time("initTimeCost");
-        return new Promise((resolve, reject) => {
-            // 基因數量大小
-            for (let i = 0; i < this.initGenSize; i++) {
-                let initCompute = this.compute.slice();
-                let initVNF = this.vnf.slice();
-                // 產生基因
-                while (initVNF.length != 0) {
-                    // 隨機選擇
-                    let computeCompute = Math.floor(Math.random() * initCompute.length);
-                    let vnfCount = Math.floor(Math.random() * initVNF.length);
-                    // 確認選擇到的物品是否可放入計算節點
-                    if (initCompute[computeCompute] >= initVNF[vnfCount]) {
-                        // 將資料放入相對應位子當作初始化基因
-                        this.initGen[i][computeCompute + 1].push(initVNF[vnfCount]);
-                        // 更新計算節點剩餘資源
-                        initCompute[computeCompute] = initCompute[computeCompute] - initVNF[vnfCount];
-                        // 將目標物體移出
-                        let index = initVNF.indexOf(initVNF[vnfCount]);
-                        if (index > -1) {
-                            initVNF.splice(index, 1);
-                        }
-                    }
-                }
-            }
-            // console.log(this.initGen);
-            console.timeEnd("initTimeCost");
-            resolve(this.arrayCopy(this.initGen));
-        });
-    }
-
-    score(initGen) {
-        console.time("scoreTimeCost");
-        return new Promise((resolve, reject) => {
-            for (let i = 0; i < initGen.length; i++) {
-                let genScore = 0;
-                let runNode = 0;
-                for (let j = 1; j <= this.compute.length; j++) {
-                    if (initGen[i][j].length > 0) {
-                        genScore = genScore + (this.sumData(initGen[i][j]) / this.compute[j - 1]);
-                        runNode = runNode + 1;
-                    }
-                }
-                let usageRate = this.compute.length - runNode
-                initGen[i][0] = this.strip(Number(genScore.toFixed(2)) + usageRate);
-            }
-            // console.log(initGen);
-            // console.log(this.compute);
-            console.timeEnd("scoreTimeCost");
-            resolve(initGen);
-        });
-    }
-
-    copulation(initGen) {
-        console.time("copulationTimeCost");
-        return new Promise((resolve, reject) => {
-            let copyInitGen = this.arrayCopy(initGen);
-            let originInitGen = this.arrayCopy(initGen);
-            // 基因數量大小
-            for (let i = 0; i < copyInitGen.length; i++) {
-                // 用於比較輸出結果是否有超過Compute能力
-                let [...initCompute] = this.compute;
-                if (i % 2 == 0) {
-                    // console.log(`交換基因池中第${i}、${i + 1}個基因`);
-                    // 隨機選擇要交換的基因
-                    let copulationPoint = Math.floor(Math.random() * initCompute.length) + 1;
-                    // console.log(`交換基因位子 : ${copulationPoint}`);
-                    // 基因交換暫存使用
-                    let cache = 0;
-                    // 交換基因使用
-                    let fatherGen = copyInitGen[i][copulationPoint];
-                    let motherGen = copyInitGen[i + 1][copulationPoint];
-                    // 保留初始基因
-                    let originFatherGen = fatherGen;
-                    let originMotherGen = motherGen;
-                    // 如果被選中的基因沒有東西將不做處理
-                    if (fatherGen.length > 0 | motherGen.length > 0) {
-                        copyInitGen[i][0] = true;
-                        if (!this.arrayCompare(fatherGen, motherGen)) {
-                            // 基因交換
-                            cache = fatherGen.slice();
-                            fatherGen = motherGen.slice();
-                            motherGen = cache;
-                            // console.log(`originFatherGen : ${JSON.stringify(originFatherGen)} \t fatherGen : ${JSON.stringify(fatherGen)}`);
-                            // console.log(`originMotherGen : ${JSON.stringify(originMotherGen)} \t motherGen : ${JSON.stringify(motherGen)}`);
-                            // 更新基因回基因池
-                            copyInitGen[i][copulationPoint] = fatherGen;
-                            copyInitGen[i + 1][copulationPoint] = motherGen;
-                            // 過濾基因，用來處理交配後重複及缺少基因問題
-                            // 父基因 => 1.需刪除deduplicationFatherGen重複基因 2.補上缺少基因deduplicationMotherGen
-                            // 母基因 => 1.需刪除deduplicationMotherGen重複基因 2.補上缺少基因deduplicationFatherGen
-                            let deduplicationFatherGen = this.arrayFilter(originMotherGen, originFatherGen);
-                            let deduplicationMotherGen = this.arrayFilter(originFatherGen, originMotherGen);
-                            // console.log(`基因交換後父基因需刪除的重複基因 => ${JSON.stringify(deduplicationFatherGen)}`);
-                            // console.log(`基因交換後母基因需刪除的重複基因 => ${JSON.stringify(deduplicationMotherGen)}`);
-                            // 處理deduplicationFatherGen
-                            for (let j = 0; j < deduplicationFatherGen.length; j++) {
-                                // 刪除父親重複基因
-                                let deleteRepeat = true;
-                                while (deleteRepeat) {
-                                    let deduplication = Math.floor(Math.random() * initCompute.length) + 1;
-                                    if ((deduplication != copulationPoint) && (this.arrayFind(copyInitGen[i][deduplication], deduplicationFatherGen[j]))) {
-                                        copyInitGen[i][deduplication] = this.arrayFilter(copyInitGen[i][deduplication], [deduplicationFatherGen[j]]);
-                                        deleteRepeat = false;
-                                        // console.log(`選擇父基因位子 : ${deduplication}`);
-                                        // console.log(`刪除 ${deduplicationFatherGen[j]} 基因`);
-                                    }
-                                }
-
-                                // 加入母親欠缺基因
-                                let addGen = true;
-                                while (addGen) {
-                                    let addGenPoint = Math.floor(Math.random() * initCompute.length) + 1;
-                                    // 不將欠缺基因加入原先位子
-                                    if (addGenPoint != copulationPoint) {
-                                        if ((this.sumData(copyInitGen[i + 1][addGenPoint]) + deduplicationFatherGen[j]) <= initCompute[addGenPoint - 1]) {
-                                            copyInitGen[i + 1][addGenPoint].push(deduplicationFatherGen[j]);
-                                            addGen = false;
-                                            // console.log(`選擇母基因位子 : ${addGenPoint}`);
-                                            // console.log(`加入 ${deduplicationFatherGen[j]} 基因`);
-                                        }
-                                    }
-                                }
-                            }
-                            // 處理deduplicationMotherGen
-                            for (let j = 0; j < deduplicationMotherGen.length; j++) {
-                                // 加入父親欠缺基因
-                                let addGen = true;
-                                while (addGen) {
-                                    let addGenPoint = Math.floor(Math.random() * initCompute.length) + 1;
-                                    // 不將欠缺基因加入原先位子
-                                    if (addGenPoint != copulationPoint) {
-                                        if ((this.sumData(copyInitGen[i][addGenPoint]) + deduplicationMotherGen[j]) <= initCompute[addGenPoint - 1]) {
-                                            copyInitGen[i][addGenPoint].push(deduplicationMotherGen[j]);
-                                            addGen = false;
-                                            // console.log(`選擇父基因位子 : ${addGenPoint}`);
-                                            // console.log(`加入 ${deduplicationMotherGen[j]} 基因`);
-                                        }
-                                    }
-                                }
-                                // 刪除母親重複基因
-                                let deleteRepeat = true;
-                                while (deleteRepeat) {
-                                    let deduplication = Math.floor(Math.random() * initCompute.length) + 1;
-                                    if ((deduplication != copulationPoint) && (this.arrayFind(copyInitGen[i + 1][deduplication], deduplicationMotherGen[j]))) {
-                                        copyInitGen[i + 1][deduplication] = this.arrayFilter(copyInitGen[i + 1][deduplication], [deduplicationMotherGen[j]]);
-                                        deleteRepeat = false;
-                                        // console.log(`選擇母基因位子 : ${deduplication}`);
-                                        // console.log(`刪除 ${deduplicationMotherGen[j]} 基因`);
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            console.timeEnd("copulationTimeCost");
-            resolve(copyInitGen);
-        });
-    }
-
-    betterGenes(initGen, copulationGen) {
-        return new Promise((resolve, reject) => {
-            let arr1 = this.arrayCopy(initGen.concat(copulationGen));
-            arr1.sort();
-            arr1.splice(0, arr1.length / 2);
-            resolve(arr1);
-        });
-    }
-
-    sumData(arr) {
-        let sum = 0;
-        for (let i = 0; i < arr.length; i++) {
-            sum += arr[i];
-        };
-        return sum;
-    }
-
-    // 解決javascript float問題
-    // 0.1 + 0.2 = 0.30000000000000004
-    strip(num, precision = 12) {
-        return +parseFloat(num.toPrecision(precision));
-    }
-
-    // 陣列比較
-    arrayCompare(arr1, arr2) {
-        return arr1.sort().toString() == arr2.sort().toString()
-    }
-
-    arrayFilter(arr1, arr2) {
-        let arrResult = arr1.slice();
-        for (let i = 0; i < arr2.length; i++) {
-            arrResult.remove(arr2[i]);
-        }
-        return arrResult;
-    }
-
-    arrayFind(arr, data) {
-        let found = arr.find(element => element == data);
-        return found == undefined ? false : true;
-    }
-
-    arrayCopy(currentArray) {
-        let newArray = currentArray.map(function (arr) {
-            return arr.slice();
-        });
-        return newArray;
+// 在基因大小中新增計算節點維度
+for (let i = 0; i < initGen.length; i++) {
+    // 新增計算維度
+    initGen[i] = new Array(compute.length + 1);
+    initGen[i][0] = [null];
+    // 在計算節點中新增放置空間
+    for (let j = 1; j <= compute.length; j++) {
+        // 新增維度
+        initGen[i][j] = new Array(0);
     }
 }
 
-module.exports = GA;
+// 複製三維陣列至新的記憶體位置
+const arrayCopy = function (currentArray) {
+    let newArray = currentArray.map(function (arr1) {
+        let newArray1 = arr1.map(function (arr2) {
+            return arr2.slice()
+        });
+        return newArray1;
+    });
+    return newArray;
+}
+
+// 陣列總和
+const sumData = function (arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    };
+    return sum;
+}
+
+// 解決javascript float問題
+// 0.1 + 0.2 = 0.30000000000000004
+const strip = function (num, precision = 12) {
+    return +parseFloat(num.toPrecision(precision));
+}
+
+// 產生初始化基因池
+const init = function () {
+    return new Promise((resolve, reject) => {
+        // 基因數量大小
+        for (let i = 0; i < initGenSize; i++) {
+            let initCompute = compute.slice();
+            let initVNF = vnf.slice();
+            // 產生基因
+            while (initVNF.length != 0) {
+                // 隨機選擇
+                let computeCompute = Math.floor(Math.random() * initCompute.length);
+                let vnfCount = Math.floor(Math.random() * initVNF.length);
+                // 確認選擇到的物品是否可放入計算節點
+                if (initCompute[computeCompute] >= initVNF[vnfCount]) {
+                    // 將資料放入相對應位子當作初始化基因
+                    initGen[i][computeCompute + 1].push(initVNF[vnfCount]);
+                    // 更新計算節點剩餘資源
+                    initCompute[computeCompute] = initCompute[computeCompute] - initVNF[vnfCount];
+                    // 將目標物體移出
+                    let index = initVNF.indexOf(initVNF[vnfCount]);
+                    if (index > -1) {
+                        initVNF.splice(index, 1);
+                    }
+                }
+            }
+        }
+        resolve(arrayCopy(initGen));
+    });
+}
+
+// 為初始化基因給予評分
+const initScore = function (data) {
+    return new Promise((resolve, reject) => {
+        let initScoreResult = arrayCopy(data);
+        for (let i = 0; i < initScoreResult.length; i++) {
+            let genScore = 0;
+            let runNode = 0;
+            for (let j = 1; j <= compute.length; j++) {
+                if (initScoreResult[i][j].length > 0) {
+                    genScore = genScore + (sumData(initScoreResult[i][j]) / compute[j - 1]);
+                    runNode = runNode + 1;
+                }
+            }
+            let usageRate = compute.length - runNode
+            initScoreResult[i][0] = [strip(Number(genScore.toFixed(2)) + usageRate)];
+        }
+        resolve(initScoreResult);
+    });
+}
+
+// 待確認
+const copulation = function (data) {
+    return new Promise((resolve, reject) => {
+        let copulation_Data = arrayCopy(data);
+        for (let i = 0; i < copulation_Data.length; i++) {
+            // 用於比較輸出結果是否有超過Compute能力
+            let [...initCompute] = compute;
+        }
+        resolve(copulation_Data);
+    });
+}
+
+async function test() {
+    let initResult = await init();
+    let initScoreGen = await initScore(initResult);
+    let cache_Data;
+    for (let i = 0; i < 100; i++) {
+        if (i == 0) {
+            let initCopulationResult = await copulation(initScoreGen);
+            cache_Data = arrayCopy(initCopulationResult);
+        }
+        let copulationResult = await copulation(cache_Data);
+        cache_Data = arrayCopy(copulationResult);
+    }
+    initGen[0][0][0] = 123;
+    cache_Data[1][0][0] = 456;
+    console.log(initResult);
+    console.log(initScoreGen);
+    console.log(cache_Data);
+    console.log(initGen);
+}
+
+test();
