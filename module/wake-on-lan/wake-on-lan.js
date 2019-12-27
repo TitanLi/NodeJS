30d847d9a0154b0086ea944300d5c59505cc9440
TitanLi <lisheng0706@gmail.com>
2019-12-27 14:46:56 +0800

    Update wake-on-lan

--- module/wake-on-lan/wake-on-lan.js
+++ module/wake-on-lan/wake-on-lan.js
@@ -1,3 +1,8 @@
+// 1. First, install ethtool:
+// $ sudo apt install ethtool
+// 2. For testing and temporarily enabling WoL: 
+// $ sudo ethtool -s eth0 wol g
+
 const wol = require('wol');
  
 wol.wake('a0:48:1c:a0:6f:29', function(err, res){
