83689be611385cdf93d755738f5bcbccfa1d88e6
TitanLi <lisheng0706@gmail.com>
2020-02-06 17:56:21 +0800

    Add get JSON key example

---
+++ method/JSON/getkey.js
@@ -0,0 +1,9 @@
+const obj = {
+    "success1":"You are welcome1",
+    "success2":"You are welcome2",
+    "success3":"You are welcome3",
+    "success4":"You are welcome4"
+};
+
+let keys = Object.keys(obj);
+console.log(keys);
