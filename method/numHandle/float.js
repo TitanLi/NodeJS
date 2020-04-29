c6633b90e8c1ebb2f07e74a85eb9ef9508325581
TitanLi <lisheng0706@gmail.com>
2020-04-29 10:08:38 +0800

    Update numHandle

--- method/numHandle/float.js
+++ method/numHandle/float.js
@@ -2,7 +2,7 @@
 // 解決javascript float問題
 // 0.1 + 0.2 = 0.30000000000000004
 const strip = function (num, precision = 12) {
-    return +parseFloat(num.toPrecision(precision));
+    return Number(num.toFixed(precision));
 }
 
 let num = 0.1 + 0.2;
